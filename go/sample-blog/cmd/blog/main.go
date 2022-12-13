712e05682245917b24b8f94baf055b2dad7fef7d
Valentin Deleplace <deleplace@google.com>
2022-12-13 14:53:02 +0100

    Type Server in package blog (distinct from main).

--- go/sample-blog/cmd/blog/main.go
+++ go/sample-blog/cmd/blog/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"os"
+
+	blog "github.com/Deleplace/sample-blog/go/sample-blog"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Serving (see response in your browser)")
-		fmt.Fprintln(w, "Hello. This is a very minimal Go webapp.")
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-	addr := os.Getenv("ADDR") + ":" + port
-	log.Printf("Listening on %s\n", addr)
-	err := http.ListenAndServe(addr, nil)
+	s := blog.NewServer()
+	err := s.Start()
 	log.Fatal(err)
 }
